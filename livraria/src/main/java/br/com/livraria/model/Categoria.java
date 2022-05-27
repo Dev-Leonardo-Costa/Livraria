b7a4519de2605147db6934656d4bb22632473104
Dev-Leonardo-Costa <devleonardo27@gmail.com>
2022-05-27 09:04:22 -0300

    inserindo as anotações para criar nossas tabelas no banco

--- livraria/src/main/java/br/com/livraria/model/Categoria.java
+++ livraria/src/main/java/br/com/livraria/model/Categoria.java
@@ -3,19 +3,24 @@ package br.com.livraria.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @EqualsAndHashCode
-public class Categoria {
+@Entity
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String descricao;
 
+    @OneToMany(mappedBy = "categoria")
     private List<Livro> livros = new ArrayList<>();
 }
--- livraria/src/main/java/br/com/livraria/model/Livro.java
+++ livraria/src/main/java/br/com/livraria/model/Livro.java
@@ -3,15 +3,24 @@ package br.com.livraria.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @EqualsAndHashCode
-public class Livro {
+@Entity
+public class Livro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
 }
