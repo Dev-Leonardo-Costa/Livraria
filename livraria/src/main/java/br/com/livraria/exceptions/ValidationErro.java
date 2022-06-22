cd21fa0673f9e6e79d7e33bb30cc7fc83a5e9e10
Leonardo Costa <devleonardo27@gmail.com>
2022-06-21 22:30:00 -0300

    removendo linhas sem utilidade

--- livraria/src/main/java/br/com/livraria/exceptions/ValidationErro.java
+++ livraria/src/main/java/br/com/livraria/exceptions/ValidationErro.java
@@ -1,12 +1,10 @@
 package br.com.livraria.exceptions;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+
+import lombok.Getter;
 
 @Getter
 public class ValidationErro extends StandardError{
