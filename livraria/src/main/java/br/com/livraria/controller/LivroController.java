8e6910b37707ea9729ccbe7be09c0d86a69e4a7f
Leonardo Costa <devleonardo27@gmail.com>
2022-06-21 22:22:05 -0300

    adicionando anotação @CrossOrigin

--- livraria/src/main/java/br/com/livraria/controller/LivroController.java
+++ livraria/src/main/java/br/com/livraria/controller/LivroController.java
@@ -13,26 +13,24 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
 
     @Autowired
     private LivroService livroService;
-
     @GetMapping("/{id}")
     public ResponseEntity<Livro> buscarPorId(@PathVariable Long id) {
         Livro obj = livroService.buscarPorId(id);
         return ResponseEntity.ok().body(obj);
     }
-
     @GetMapping
     public ResponseEntity<List<LivroDTO>> buscarTodos(@RequestParam(value = "categoria", defaultValue = "0") Long id_cat) {
         List<Livro> lista = livroService.buscarTodos(id_cat);
         List<LivroDTO> listaDTO = lista.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listaDTO);
     }
-
     @PutMapping(value = "/{id}")
     public ResponseEntity<Livro> atulizar(@PathVariable Long id, @Valid @RequestBody Livro obj) {
         Livro novoLivro = livroService.atualizar(id, obj);
@@ -44,7 +42,6 @@ public class LivroController {
         Livro novoLivro = livroService.atualizar(id, obj);
         return ResponseEntity.ok().body(novoLivro);
     }
-
     @PostMapping
     public ResponseEntity<Livro> salvar(@RequestParam(value = "categoria", defaultValue = "0") Long id_cat, @Valid @RequestBody Livro obj) {
         Livro novoLivro = livroService.salvar(id_cat, obj);
